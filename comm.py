c1ae556dabb9fd5966b8f331bc39ffd0d0239e75
alexk101 <alexkiefer51@gmail.com>
2025-04-27 00:56:01 -0400

    logging for distributed

--- comm.py
+++ comm.py
@@ -1,9 +1,18 @@
 import os
+import torch
 import torch.distributed as dist
 import datetime
 
 def init_process_group():
     world_size = int(os.getenv("WORLD_SIZE", 1))
+    print(f"WORLD_SIZE: {world_size}")
+    print(f"RANK: {os.getenv('RANK')}")
+    print(f"LOCAL_RANK: {os.getenv('LOCAL_RANK')}")
+    print(f"ROCR_VISIBLE_DEVICES: {os.getenv('ROCR_VISIBLE_DEVICES')}")
+
+    print(f"torch.cuda.is_available(): {torch.cuda.is_available()}")
+    print(f"torch.cuda.current_device(): {torch.cuda.current_device()}")
+    print(f"torch.cuda.device_count(): {torch.cuda.device_count()}")
 
     if world_size > 1:
         dist.init_process_group(
